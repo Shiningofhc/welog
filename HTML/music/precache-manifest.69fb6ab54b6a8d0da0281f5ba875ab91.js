a60beadd21baa84c86fde1df252d753985f1211f
Amnesia-f <3586454827@qq.com>
2021-05-05 15:42:29 +0800

    Site updated: 2021-05-05 15:41:36

---
+++ HTML/music/precache-manifest.69fb6ab54b6a8d0da0281f5ba875ab91.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c0c3beac6e29739011",
+    "url": "css/app.893c7cfa.css"
+  },
+  {
+    "revision": "69971c878735a23bfbff",
+    "url": "css/chunk-vendors.c0f0ef1d.css"
+  },
+  {
+    "revision": "3a2fb22a986910b24988855242a487c6",
+    "url": "f2c0e619ef56eb3afee9.worker.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "f7c37536455e41c53020e2332e7ed314",
+    "url": "index.html"
+  },
+  {
+    "revision": "2d12777f2703612307ff4a12f1b21899",
+    "url": "ixarea-stats.js"
+  },
+  {
+    "revision": "19c0c3beac6e29739011",
+    "url": "js/app.13c8bc83.js"
+  },
+  {
+    "revision": "69971c878735a23bfbff",
+    "url": "js/chunk-vendors.8370ac16.js"
+  },
+  {
+    "revision": "02995355b96ddf2519cd49f8aa73bb46",
+    "url": "loader.js"
+  },
+  {
+    "revision": "523b1a2eae8cb533fa6bd73831308f09",
+    "url": "static/kgm.mask"
+  },
+  {
+    "revision": "cd1d395410107c66b4534ec93f0073d3",
+    "url": "web-manifest.json"
+  }
+]);
